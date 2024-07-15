01cfc7c7dba34f67af433adcd1a6fee733e015c9
kotekpsotek <siematumichael@protonmail.com>
2024-07-15 02:37:42 +0200

    arrival -> Save book in database

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
-const dotenv = require("dotenv");
 const express = require("express");
 const app = express();
+
+// Load environmental variables to process.env
+require("dotenv").config();
+
+// User configuration sides
 const userActions = require("./addons/userActions");
+const { modelBooks } = require("./database/models");
 
-dotenv.config({ encoding: "utf-8" });
-
-// Sign-in/Sign-up user there
+app.use(express.json());
 app.use("/user-actions", userActions);
 
 // Book library handling
+app.post("/save-book", (req, res) => {
+    if (req.body.password && req.body.book_data) {
+        const { password, book_data } = req.body;
+    
+        if (password == process.env.passwordAllowSave) {
+            if (book_data.title && book_data.author && book_data.isbn) {
+                modelBooks.create({ 
+                    title: book_data.title, 
+                    author: book_data.author, 
+                    isbn: book_data.isbn 
+                });
+                res.sendStatus(202);
+            }
+            else res.sendStatus(403);
+        }
+        else res.sendStatus(401);
+    }
+    else res.sendStatus(403);
+});
+
 app.get("/get-books/:base_on/:id", async (req, res) => {
     const { base_on, id } = req.params;
 
@@ -19,7 +42,7 @@ app.get("/get-books/:base_on/:id", async (req, res) => {
         // Give back all books avaiable in the store
         case "all":
             {
-                
+
             }
         break;
 
