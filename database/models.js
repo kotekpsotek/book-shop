ba0a2488de9fbadb1ca9754360c0678216963f88
kotekpsotek <siematumichael@protonmail.com>
2024-07-16 14:59:46 +0200

    arrival -> Basic create account functionality

--- database/models.js
+++ database/models.js
@@ -1,10 +1,18 @@
 const mongoose = require("mongoose");
 const bookSchema = require("./schema/books");
+const usersSchema = require("./schema/user");
 
+// One connection = one responsibility
+// .. Books
 const con1 = mongoose.createConnection("mongodb://localhost:27017/book-store"); // FIXME: in production environment your database should be protected by password, login etc..
 const modelBooks = con1.model("booksModel", bookSchema.defualt, "books"); // 1 Model can be assigned to utmost 1 connection
 
+// .. Users
+const con2 = mongoose.createConnection("mongodb://localhost:27017/book-store")
+const modelUsers = con2.model("usersModel", usersSchema, "users");
+
 module.exports = {
     con1,
-    modelBooks
+    modelBooks,
+    modelUsers
 }
---
+++ database/schema/user.js
@@ -0,0 +1,12 @@
+const { Schema } = require("mongoose");
+const { v7: uuid } = require("uuid");
+
+const user = new Schema({
+    uuid: { type: String, default: () => uuid(), unique: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true, minLength: 8, maxLength: 500 },
+    arrivalDate: { type: Date , default: () => new Date() }
+})
+
+module.exports = user;
